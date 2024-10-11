ce33bee9e176919b941b7a770bfd289f0da1eab6
Prince Prajapati <princeprajapati8948@gmail.com>
2024-10-11 11:33:38 +0530

    functiond call

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//when app is a function so we can create onw functions 
+//lets create and render it
+
+function MyApp() {
+  <div><h1>this is custom app</h1></div>
+}
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+    <MyApp />
+  </>
+
+
+
 )
