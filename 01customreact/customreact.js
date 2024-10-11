553c64526391043144ee718eb08c39305254b9dd
Prince Prajapati <princeprajapati8948@gmail.com>
2024-10-11 11:16:03 +0530

    how to create custom react elements in react

--- 01customreact/customreact.js
+++ 01customreact/customreact.js
@@ -1,5 +1,5 @@
 
-function customRender(reactElements, container){
+function customRender(reactElements, container) {
     const domEle = document.createElement(reactElements.type)
     domEle.innerHTML = reactElements.children;
     domEle.setAttribute('href', reactElements.props.href);
@@ -7,13 +7,30 @@ function customRender(reactElements, container){
     container.appendChild(domEle);
 }
 const reactElements = {
-    type:'a',
-    props:{
-        href:'www.google.com',
-        target:'_blank',
+    type: 'a',
+    props: {
+        href: 'www.google.com',
+        target: '_blank',
     },
     children: 'Goto Google',
+}
+
+//lets create another elements 
+const anotherElement = {
+    type: 'img',
+    props: {
+        width: '500px',
+        src: 'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg',
+        alt: 'Error to img load'
+    }
+}
+function imageRender(anotherElement, container) {
+    const imgDom = document.createElement(anotherElement.type)
+    imgDom.setAttribute('src', anotherElement.props.src)
+    imgDom.setAttribute('width', anotherElement.props.width)
+    container.appendChild(imgDom)
 }
 const mainContainer = document.querySelector('#root')
 
 customRender(reactElements, mainContainer)
+imageRender(anotherElement, mainContainer)
